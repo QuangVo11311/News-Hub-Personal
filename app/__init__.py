284841874234219473657166d35f87fa5686f6f6
Võ Đăng Quang <94774447+QuangVo11311@users.noreply.github.com>
2025-02-06 10:33:27 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
 # Khởi tạo SQLAlchemy
 db = SQLAlchemy()
+
+# Khởi tạo Bcrypt và JWTManager
+bcrypt = Bcrypt()
+jwt = JWTManager()
 
 
 def create_app():
@@ -13,6 +19,8 @@ def create_app():
 
     # Khởi tạo database với app
     db.init_app(app)
+    bcrypt.init_app(app)  # Khởi tạo bcrypt
+    jwt.init_app(app)  # Khởi tạo JWT
 
     # Import và đăng ký các route
     from app.routes import main
